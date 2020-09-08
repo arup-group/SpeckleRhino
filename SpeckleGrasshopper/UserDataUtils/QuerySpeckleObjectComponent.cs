07c57ad57a4c06c56a96eccc9dfd8ef73385ea43
Megan Karbowski <Megan.Karbowski@arup.com>
2020-09-08 15:01:28 -0700

    updated description

--- SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
+++ SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
@@ -22,7 +22,7 @@ namespace SpeckleGrasshopper
     /// Initializes a new instance of the MyComponent1 class.
     /// </summary>
     public QuerySpeckleObjectComponent()
-      : base("Query Speckle Object test!!!!!!!!", "GNV",
+      : base("Query Speckle Object", "QSO",
           "Gets a value from a dictionary by string of concatenated keys. \n For example, 'prop.subprop.subsubprop'.",
           "Speckle", "Special")
     {
