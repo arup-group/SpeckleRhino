c29d80edf665b551def202061248af8f1e47d7f6
Megan Karbowski <Megan.Karbowski@arup.com>
2020-09-11 00:24:13 -0700

    working on checking if the input is a nested value

--- SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
+++ SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
@@ -66,18 +66,38 @@ namespace SpeckleGrasshopper
       if (!DA.GetData(1, ref key))
         return;
 
+      /// Split user input path into pieces
+      /// Turtle.smallTurtle.microTurtle -> [Turtle, smallTurtle, microTurtle]
+      string[] keySplit = key.Split('.');
+      //System.Diagnostics.Debug.WriteLine($"<{keySplit[1]}>");
       var propertyDict = speckleObject.Properties;
+      //System.Diagnostics.Debug.WriteLine($"<{propertyDict}>");
 
-      if (propertyDict.ContainsKey(key))
+      /// Use first part of path[Turtle] to execute existing logic to return the top-level object
+      if (propertyDict.ContainsKey(keySplit[0]))
       {
-        DA.SetData(0, propertyDict[key]);
-        return;
+        var topLevelObj = propertyDict[keySplit[0]];
+        
+        /// Check if the object is a Dictionary<string, object>
+        if (topLevelObj is Dictionary<string, object>)
+        {
+          /// If so use second part of path [smallTurtle] to access the relevant key-value pair
+          DA.SetData(0, propertyDict[keySplit[1]]);
+          return;
+        }
+        else
+        {
+          /// Else use reflection to check if the object has the second part of the path as a property
+          var output = speckleObject.GetType().GetProperty(keySplit[1]).GetValue(speckleObject, null);
+          DA.SetData(0, output);
+        }
+        
       }
       else
       {
         try
         {
-          var output = speckleObject.GetType().GetProperty(key).GetValue(speckleObject, null);
+          var output = speckleObject.GetType().GetProperty(keySplit[0]).GetValue(speckleObject, null);
           DA.SetData(0, output);
         }
         catch (System.NullReferenceException e)
@@ -85,6 +105,9 @@ namespace SpeckleGrasshopper
           AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Could not find a parameter with that key on the input object");
         }
       }
+
+      /// Repeat until no more path parts exist
+      /// Return retrieved object to user
 
 
     }
