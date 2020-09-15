7b7e2b7c25bd82e08978d840bc6478f4f12795b3
Megan Karbowski <Megan.Karbowski@arup.com>
2020-09-14 21:40:55 -0700

    working on checking if input is a dictionary, returns correct value if not a dictionary

--- SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
+++ SpeckleGrasshopper/UserDataUtils/QuerySpeckleObjectComponent.cs
@@ -67,44 +67,74 @@ namespace SpeckleGrasshopper
         return;
 
       /// Split user input path into pieces
-      /// Turtle.smallTurtle.microTurtle -> [Turtle, smallTurtle, microTurtle]
       string[] keySplit = key.Split('.');
-      //System.Diagnostics.Debug.WriteLine($"<{keySplit[1]}>");
       var propertyDict = speckleObject.Properties;
-      //System.Diagnostics.Debug.WriteLine($"<{propertyDict}>");
 
-      /// Use first part of path[Turtle] to execute existing logic to return the top-level object
-      if (propertyDict.ContainsKey(keySplit[0]))
+      var topLevelObj = propertyDict[keySplit[0]];
+
+      if (topLevelObj is Dictionary<string, object>)
       {
-        var topLevelObj = propertyDict[keySplit[0]];
-        
-        /// Check if the object is a Dictionary<string, object>
-        if (topLevelObj is Dictionary<string, object>)
+        DA.SetData(0, propertyDict[keySplit[1]]);
+        return;
+      }
+      else
+      {
+        if (propertyDict.ContainsKey(keySplit[0]))
         {
-          /// If so use second part of path [smallTurtle] to access the relevant key-value pair
-          DA.SetData(0, propertyDict[keySplit[1]]);
+          DA.SetData(0, propertyDict[keySplit[0]]);
           return;
         }
         else
         {
-          /// Else use reflection to check if the object has the second part of the path as a property
-          var output = speckleObject.GetType().GetProperty(keySplit[1]).GetValue(speckleObject, null);
-          DA.SetData(0, output);
+          try
+          {
+            var output = speckleObject.GetType().GetProperty(keySplit[0]).GetValue(speckleObject, null);
+            DA.SetData(0, output);
+          }
+          catch (System.NullReferenceException e)
+          {
+            AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Could not find a parameter with that key on the input object");
+          }
         }
-        
+
       }
-      else
-      {
-        try
-        {
-          var output = speckleObject.GetType().GetProperty(keySplit[0]).GetValue(speckleObject, null);
-          DA.SetData(0, output);
-        }
-        catch (System.NullReferenceException e)
-        {
-          AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Could not find a parameter with that key on the input object");
-        }
-      }
+
+
+      
+
+
+      /// Use first part of path[Turtle] to execute existing logic to return the top-level object
+      //if (propertyDict.ContainsKey(keySplit[0]))
+      //{
+      //  var topLevelObj = propertyDict[keySplit[0]];
+
+      //  /// Check if the object is a Dictionary<string, object>
+      //  if (topLevelObj is Dictionary<string, object>)
+      //  {
+      //    /// If so use second part of path [smallTurtle] to access the relevant key-value pair
+      //    DA.SetData(0, propertyDict[keySplit[1]]);
+      //    return;
+      //  }
+      //  else
+      //  {
+      //    /// Else use reflection to check if the object has the second part of the path as a property
+      //    var output = speckleObject.GetType().GetProperty(keySplit[0]).GetValue(speckleObject, null);
+      //    DA.SetData(0, output);
+      //  }
+
+      //}
+      //else
+      //{
+      //  try
+      //  {
+      //    var output = speckleObject.GetType().GetProperty(keySplit[0]).GetValue(speckleObject, null);
+      //    DA.SetData(0, output);
+      //  }
+      //  catch (System.NullReferenceException e)
+      //  {
+      //    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Could not find a parameter with that key on the input object");
+      //  }
+      //}
 
       /// Repeat until no more path parts exist
       /// Return retrieved object to user
